84a04c99ae5f36c26a4bf2e823e038b1ae7bae70
kocoronoblue <kocoronoblue@gmail.com>
2019-05-15 09:31:12 +0900

    ああ

--- python_lesson/script.py
+++ python_lesson/script.py
@@ -1,38 +1,29 @@
-# or -> 「条件1 or 条件2」のときはorを使う
-time = 15
-if time == 10 or time == 15:
-    print('おやつの時間ですよ')
+import gspread
+import json
 
+#ServiceAccountCredentials：Googleの各サービスへアクセスできるservice変数を生成します。
+from oauth2client.service_account import ServiceAccountCredentials
 
-# not -> 「not条件式」にすると、条件式がTrueであれば「False」に、「False」であれば「True」になります。
-time = 9
-if not time == 18:
-    print('まだ帰れねーって')
+#2つのAPIを記述しないとリフレッシュトークンを3600秒毎に発行し続けなければならない
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
-if time == 9:
-    print('帰って良し')
+#認証情報設定
+#ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
+credentials = ServiceAccountCredentials.from_json_keyfile_name('LT-entertainment-1796cc6507c0.json', scope)
 
+#OAuth2の資格情報を使用してGoogle APIにログインします。
+gc = gspread.authorize(credentials)
 
+#共有設定したスプレッドシートキーを変数[SPREADSHEET_KEY]に格納する。
+SPREADSHEET_KEY = '1MTE_DETO7laU6S0uvvLK9XnocAMrvjlhRO-Kj30eL8g'
 
+#共有設定したスプレッドシートのシート1を開く
+worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
 
+#受け取るセルの値
+import_value = int(worksheet.acell('C2').value)
+import_value2 = int(worksheet.acell('C3').value)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# inputカウントを使う
-apple_price = 300
-input_count = input('購入するりんごの個数を入力して:')
-total_price = apple_price * int(input_count)
-print('購入するりんごの個数は'+input_count+'個です')
-print('支払い金額は'+str(total_price)+'円です')
+#受け取ったセルの値を合計した値をH2セルに表示させる
+export_value = import_value + import_value2
+worksheet.update_cell(2,8, export_value)
